a58fc2ea293f7fc83f666ac2b5dd0e2a04002817
ElChameleonio <76488348+ElChameleonio@users.noreply.github.com>
2021-01-19 15:00:17 +0300

    начало контроллера(сделать сервис)

---
+++ server/src/main/java/com/server/task/repo/TaskRepositoryImp.java
@@ -0,0 +1,18 @@
+//Нихуя не понимаю как это работает
+
+package com.server.task.repo;
+
+import com.server.task.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.server.task.model.Task;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, String> {
+
+}
