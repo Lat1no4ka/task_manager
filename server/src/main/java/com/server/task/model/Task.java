353892f709b5ba6196ccdff36e6c4da6e3f9ed08
ElChameleonio <76488348+ElChameleonio@users.noreply.github.com>
2021-01-21 16:25:17 +0300

    Подстроил модель на бэке под фронт (не забудь удалить старую таблицу)

--- server/src/main/java/com/server/task/model/Task.java
+++ server/src/main/java/com/server/task/model/Task.java
@@ -12,20 +12,23 @@ public class Task implements Serializable {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "head_id", nullable = false)
-    private String headid;
-
-    @Column(name = "emp_id", nullable = false)
-    private String empid;
-
     @Column(name = "task_name")
     private String taskname;
 
     @Column(name = "task_desc")
     private String taskdesc;
 
+    @Column(name = "beg_date")
+    private String begdate;
+
     @Column(name = "exp_date")
     private String expdate;
+
+    @Column(name = "head_id", nullable = false)
+    private String headid;
+
+    @Column(name = "emp_id", nullable = false)
+    private String empid;
 
     @Column(name = "task_priority")
     private String taskpriority;
@@ -33,13 +36,14 @@ public class Task implements Serializable {
     @Column(name = "task_status")
     private String taskstatus;
 
-    public Task(Long id, String headid, String empid, String taskname, String taskdesc, String expdate, String taskpriority, String taskstatus) {
+    public Task(Long id, String taskname,String begdate, String expdate, String taskdesc, String headid, String empid, String taskpriority, String taskstatus) {
         this.id = id;
-        this.headid = headid;
-        this.empid = empid;
         this.taskname = taskname;
         this.taskdesc = taskdesc;
+        this.begdate = begdate;
         this.expdate = expdate;
+        this.headid = headid;
+        this.empid = empid;
         this.taskpriority = taskpriority;
         this.taskstatus = taskstatus;
     }
@@ -52,11 +56,12 @@ public class Task implements Serializable {
 
     public Task() {
         this.id = null;
-        this.headid = null;
-        this.empid = null;
         this.taskname = null;
         this.taskdesc = null;
+        this.begdate = null;
         this.expdate = null;
+        this.headid = null;
+        this.empid = null;
         this.taskpriority = null;
         this.taskstatus = null;
     }
@@ -65,24 +70,24 @@ public class Task implements Serializable {
         this.id = id;
     }
 
+    public void setTaskname(String taskname) { this.taskname = taskname; }
+
+    public void setTaskdesc(String taskdesc) { this.taskdesc = taskdesc; }
+
+    public void setBegdate(String expdate) {
+        this.begdate = begdate;
+    }
+
+    public void setExpdate(String expdate) {
+        this.expdate = expdate;
+    }
+
     public void setHeadid(String headid) {
         this.headid = headid;
     }
 
     public void setEmpid(String empid) {
         this.empid = empid;
-    }
-
-    public void setTaskname(String taskname) {
-        this.taskname = taskname;
-    }
-
-    public void setTaskdesc(String taskdesc) {
-        this.taskdesc = taskdesc;
-    }
-
-    public void setExpdate(String expdate) {
-        this.expdate = expdate;
     }
 
     public void setTaskpriority(String taskpriority) {
@@ -97,14 +102,6 @@ public class Task implements Serializable {
         return id;
     }
 
-    public String getHeadid() {
-        return headid;
-    }
-
-    public String getEmpid() {
-        return empid;
-    }
-
     public String getTaskname() {
         return taskname;
     }
@@ -113,8 +110,20 @@ public class Task implements Serializable {
         return taskdesc;
     }
 
+    public String getBegdate() {
+        return begdate;
+    }
+
     public String getExpdate() {
         return expdate;
+    }
+
+    public String getHeadid() {
+        return headid;
+    }
+
+    public String getEmpid() {
+        return empid;
     }
 
     public String getTaskpriority() {
