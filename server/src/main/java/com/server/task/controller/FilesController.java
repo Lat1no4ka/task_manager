1e8248729637d42d4a145bc2892a228968dbb4a8
ElChameleonio <76488348+ElChameleonio@users.noreply.github.com>
2021-04-17 13:17:33 +0300

    Прикрепление нескольких файлов (через list) + хеширование названий файлов, чтоб конфликтов не было при загрузке

--- server/src/main/java/com/server/task/controller/FilesController.java
+++ server/src/main/java/com/server/task/controller/FilesController.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 import com.server.task.model.Task;
@@ -26,12 +29,14 @@ public class FilesController {
     @Autowired
     FilesRepository filesRepository;
 
+    //одиночная загрузка - на всякий случай оставляю
     @RequestMapping(value = "/uploadFile", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Files fileUpload(@RequestParam("file") MultipartFile file, @RequestParam("taskId") Long id) throws IOException {
         Files files = new Files();
         files.setFileName(file.getOriginalFilename());
+        String hashFilename = (UUID.randomUUID()).toString();
         //--------------------------------!!!!---------------------------------
-        File convertFile = new File("src\\main\\resources\\uploads\\"+file.getOriginalFilename()); //Сюда напишите путь до папки, куда нужно сохранить
+        File convertFile = new File("src\\main\\resources\\uploads\\"+hashFilename); //Сюда напишите путь до папки, куда нужно сохранить
         //--------------------------------!!!!---------------------------------
         convertFile.createNewFile();
         FileOutputStream fout = new FileOutputStream(convertFile);
@@ -44,6 +49,33 @@ public class FilesController {
 
         return files;
     }
+
+
+    //множественная загрузка
+    @RequestMapping(value = "/uploadFiles", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public List<Files> filesUpload(@RequestParam("file") List<MultipartFile> multiPartFiles, @RequestParam("taskId") Long id) throws IOException {
+        List<Files> fileList = new ArrayList<>();
+
+        for (MultipartFile mPFile : multiPartFiles) {
+            Files file = new Files();
+            file.setFileName(mPFile.getOriginalFilename());
+            String hashFilename = (UUID.randomUUID()).toString();
+            File convertFile = new File("src\\main\\resources\\uploads\\" + hashFilename);
+            convertFile.createNewFile();
+            FileOutputStream fout = new FileOutputStream(convertFile);
+            fout.write(mPFile.getBytes());
+            fout.close();
+            file.setFilePath(convertFile.toString());
+            file.setTaskId(id);
+            fileList.add(file);
+        }
+
+        filesRepository.saveAll(fileList);
+
+        return fileList;
+    }
+
+
 
     @RequestMapping(value = "/downloadFile", method = RequestMethod.POST, headers = {"Content-type=application/json"})
     public ResponseEntity<Object> downloadFile(@RequestBody Files files) throws IOException  {
