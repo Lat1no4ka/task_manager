0f46d7f44993ce88975cc1f437434b20366da9f5
ElChameleonio <76488348+ElChameleonio@users.noreply.github.com>
2021-05-12 09:42:56 +0300

    Оповещения + восстановление пароля

--- server/src/main/java/com/server/task/controller/RestoreController.java
+++ server/src/main/java/com/server/task/controller/RestoreController.java
@@ -1,18 +1,21 @@
 package com.server.task.controller;
 
+import java.io.IOException;
 import java.security.SecureRandom;
+import com.server.task.model.Files;
+import com.server.task.model.User;
+import com.server.task.model.entity.TaskEntity;
+import com.server.task.model.entity.UserEntity;
+import com.server.task.repo.TaskEntityRepository;
+import com.server.task.repo.UserEntityRepository;
 import com.server.task.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.FactoryBean;
-import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.Random;
 
 
@@ -22,43 +25,69 @@ import java.util.Random;
 @ResponseBody
 public class RestoreController {
 
+    @Autowired
+    UserEntityRepository userEntityRepository;
+    @Autowired
+    TaskEntityRepository taskEntityRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @RequestMapping(value={"/SendCode"}, method= RequestMethod.POST, headers = {"Content-type=application/json"})
-    public static void SendCode( String[] args )
-    {
-        /*
-        SecureRandom random = new SecureRandom();
-        byte bytes[] = new byte[8];
-        random.nextBytes(bytes);
-        String code = random.toString();
-        return code;
-        ^ более безопасный
-        */
-        int min = 1000;
-        int max = 9999;
+    //Отправка временного пароля
+    @RequestMapping(value={"/sendCode"}, method= RequestMethod.POST, headers = {"Content-type=application/json"})
+    public String SendCode (@RequestBody User user) throws IOException {
+        String userMail = (userRepository.findById(user.getId())).getEmail();
+        User usr = userRepository.findById(user.getId());
+        int min = 100000;
+        int max = 999999;
         int diff = max - min;
         Random random = new Random();
         int i = random.nextInt(diff + 1);
         i += min;
-
         try (GenericXmlApplicationContext context = new GenericXmlApplicationContext()) {
             context.load("classpath:applicationContext.xml");
             context.refresh();
             JavaMailSender mailSender = context.getBean("mailSender", JavaMailSender.class);
             SimpleMailMessage templateMessage = context.getBean("templateMessage", SimpleMailMessage.class);
-
             SimpleMailMessage mailMessage = new SimpleMailMessage(templateMessage);
-
-            mailMessage.setTo("dashkovtemk@mail.ru");//адрес получателя
-
-            mailMessage.setText("Забыл пароль блять? Ну вот нахуй ты такой тупой-то? Ладно, введи этот код: " + i + "\nЕще раз забудешь свой ебаный пароль - я самоуничтожу твой компьютер");
+            mailMessage.setTo(userMail);
+            mailMessage.setText("Ваш временный пароль  " + i + "\n Не забудьте заменить его во вкладке профиля");
+            String newPass = ""+i;
+            usr.setPassword(bCryptPasswordEncoder.encode(newPass));
+            userRepository.save(usr);
             try {
                 mailSender.send(mailMessage);
             } catch (MailException mailException) {
                 mailException.printStackTrace();
             }
         }
+        return "Проверьте почту";
+    }
 
+    //Отправляет оповещение о новой задаче. Ловит id задачи
+    @RequestMapping(value={"/sendMessage"}, method= RequestMethod.POST, headers = {"Content-type=application/json"})
+    public String SendMessage (@RequestBody TaskEntity task) throws IOException {
+
+        TaskEntity tsk = taskEntityRepository.findById(task.getId());
+        UserEntity user = tsk.getEmployee();
+        UserEntity author = tsk.getAuthor();
+        String userMail = (userEntityRepository.findById(user.getId())).getEmail();
+        try (GenericXmlApplicationContext context = new GenericXmlApplicationContext()) {
+            context.load("classpath:applicationContext.xml");
+            context.refresh();
+            JavaMailSender mailSender = context.getBean("mailSender", JavaMailSender.class);
+            SimpleMailMessage templateMessage = context.getBean("templateMessage", SimpleMailMessage.class);
+            SimpleMailMessage mailMessage = new SimpleMailMessage(templateMessage);
+            mailMessage.setTo(userMail);
+            mailMessage.setText("Пользователь "+author.getFirstName()+" "+author.getLastName()+" Назначил вам новую задачу: "+tsk.getTaskName() + "\n Срок выполнения с "+ tsk.getBegDate()+" по "+tsk.getEndDate());
+            try {
+                mailSender.send(mailMessage);
+            } catch (MailException mailException) {
+                mailException.printStackTrace();
+            }
+        }
+        return "Сообщение отправлено";
     }
 
 
