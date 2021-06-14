5ac0fba9f2b8894aa014ff9bda5ca2b74cf1b06c
maximka <maximkv98@gmail.com>
2021-06-14 18:26:07 +0700

    Профиль

--- client/src/components/profile/modal-update-user.js
+++ client/src/components/profile/modal-update-user.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect, Text } from "react";
 
 import { Button, Modal } from 'react-bootstrap';
 import { useDispatch, useSelector } from "react-redux";
+import { Cursor, Eye } from 'react-bootstrap-icons'
 import { useHttp } from "../../hooks/http.hook";
 import { userActions } from "../../redux/user/action";
 
 
 const ModalUpdateUser = () => {
-  
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -15,120 +16,130 @@ const ModalUpdateUser = () => {
 
   const { loading, error, request } = useHttp();
 
-     const [userNameinf, setUserName] = useState("");
-     const [firstNameinf, setFirstName] = useState("");
-     const [lastnameinf, setLastName] = useState("");
-     const [emailinf, setEmail] = useState("");
-     const [userInfo, setUserInfo] = useState(0);
-     const [roleinf, setRole] = useState("");
-
-     const [userPassword, setUserPassword] = useState("")
-
-
-     const [form, setForm] = useState({
-      id: null,
-      userName: undefined,
-      firstName: undefined,
-      lastName: undefined,
-      email: undefined,
-      password: undefined,
-      newPassword: undefined,
-  
-    });
+  const [userNameinf, setUserName] = useState("");
+  const [firstNameinf, setFirstName] = useState("");
+  const [lastnameinf, setLastName] = useState("");
+  const [emailinf, setEmail] = useState("");
+  const [userInfo, setUserInfo] = useState(0);
+  const [roleinf, setRole] = useState("");
+  const [showPass, setShowPass] = useState(false);
+  const [showNewPass, setShowNewPass] = useState(false);
+  const [userPassword, setUserPassword] = useState("")
 
 
-     useEffect(() => {
-      getInfo();
+  const [form, setForm] = useState({
+    id: null,
+    userName: undefined,
+    firstName: undefined,
+    lastName: undefined,
+    email: undefined,
+    password: undefined,
+    newPassword: undefined,
+
+  });
+
+
+  useEffect(() => {
+    getInfo();
   }, []);
 
-    const getInfo = async () => {
-      const userData = JSON.parse(localStorage.getItem("userData"));
-      try {
-          const body = [
-              {
-                  "id": userData.userId
-              }
+  const getInfo = async () => {
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    try {
+      const body = [
+        {
+          "id": userData.userId
+        }
 
-          ]
+      ]
 
-         
-          const data = await request(`${process.env.REACT_APP_API_URL}/listUsers`, "POST", JSON.stringify(body));
-          const userInfo = data[0];
-          setUserInfo(userInfo);
-          setEmail(userInfo.email);
-          setFirstName(userInfo.firstName);
-          setLastName(userInfo.lastName);
-          setUserName(userInfo.userName);
-          setForm({ ...form, id: userData.userId });
-      } catch (error) {
-          console.log(error);
-      }
+
+      const data = await request(`${process.env.REACT_APP_API_URL}/listUsers`, "POST", JSON.stringify(body));
+      const userInfo = data[0];
+      setUserInfo(userInfo);
+      setEmail(userInfo.email);
+      setFirstName(userInfo.firstName);
+      setLastName(userInfo.lastName);
+      setUserName(userInfo.userName);
+      setForm({ ...form, id: userData.userId });
+    } catch (error) {
+      console.log(error);
+    }
   };
-  
+
   const sendForm = async () => {
 
- 
-    try {
-        const data = await request(`${process.env.REACT_APP_API_URL}/alterUser`, "POST", JSON.stringify({ ...form }));
-    } catch (error) {
-        console.log(error);
-    }
-    
-    refreshPage();
-  
-    handleClose();
-};
 
-function refreshPage(){ 
-  window.location.reload(); 
-}
+    try {
+      const data = await request(`${process.env.REACT_APP_API_URL}/alterUser`, "POST", JSON.stringify({ ...form }));
+    } catch (error) {
+      console.log(error);
+    }
+
+    refreshPage();
+
+    handleClose();
+  };
+
+  function refreshPage() {
+    window.location.reload();
+  }
 
   return (
     <>
       <Button variant="secondary" onClick={handleShow}>
         Редактировать профиль
-        </Button>
+      </Button>
 
       <Modal show={show} onHide={handleClose}
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
         centered>
         <Modal.Header closeButton>
-          <Modal.Title>Настройки</Modal.Title>
+          <Modal.Title>Редактировать профиль</Modal.Title>
         </Modal.Header>
         <Modal.Body>
 
-          <form className="d-flex row">
+          <form className="d-flex row" onSubmit={() => sendForm()}>
             <div className="form-group col-6">
-              <label>Никнейм</label>
-              <input type="value" className="form-control" id="userName" placeholder="" defaultValue = {userNameinf} onChange={(e) => setForm({ ...form, userName: e.target.value })}></input>            </div>
+              <label>Логин</label>
+              <input type="value" className="form-control" id="userName" placeholder="" defaultValue={userNameinf} onChange={(e) => setForm({ ...form, userName: e.target.value })}></input>            </div>
             <div className="form-group col-6">
               <label>Email</label>
-              <input type="value" className="form-control" id="email" placeholder="" defaultValue = {emailinf} onChange={(e) => setForm({ ...form, email: e.target.value })}></input>            </div>
+              <input type="value" className="form-control" id="email" placeholder="" defaultValue={emailinf} onChange={(e) => setForm({ ...form, email: e.target.value })}></input>            </div>
             <div className="form-group col-6">
               <label>Фамилия</label>
-              <input type="value" className="form-control" id="lastName" placeholder="" defaultValue = {lastnameinf} onChange={(e) => setForm({ ...form, lastName: e.target.value })}></input>            </div>
-           <div className="form-group col-6">
+              <input type="value" className="form-control" id="lastName" placeholder="" defaultValue={lastnameinf} onChange={(e) => setForm({ ...form, lastName: e.target.value })}></input>            </div>
+
+            <div className="form-group col-6">
               <label>Старый пароль</label>
-              <input type="password" className="form-control" id="password"  onChange={(e) => setForm({ ...form, password: e.target.value })}></input>            </div>
-              <div className="form-group col-6">
+              <div className="input-group">
+                <input type={showPass ? "text" : "password"} pattern=".{8,}" className="form-control" id="password" placeholder="" value={form.password}
+                  onChange={(e) => setForm({ ...form, password: e.target.value })}></input>
+                <span className="input-group-text" style={{ cursor: "pointer" }} id="basic-addon2"
+                  onMouseUp={e => setShowPass(false)} onMouseDown={e => setShowPass(true)}><Eye size={20} /></span>
+              </div>
+            </div>
+            <div className="form-group col-6">
               <label>Имя</label>
-              <input type="value" className="form-control" id="firstName" placeholder="" defaultValue = {firstNameinf} onChange={(e) => setForm({ ...form, firstName: e.target.value })}></input>            </div>
-              <div className="form-group col-6">
+              <input type="value" className="form-control" id="firstName" placeholder="" defaultValue={firstNameinf} onChange={(e) => setForm({ ...form, firstName: e.target.value })}></input>            </div>
+
+            <div className="form-group col-6">
               <label>Новый пароль</label>
-              <input type="password" className="form-control" id="newPassword"  onChange={(e) => setForm({ ...form, newPassword: e.target.value })}></input>            </div>
-            
+              <div className="input-group">
+                <input type={showNewPass ? "text" : "password"} pattern=".{8,}" className="form-control" id="password" placeholder="" value={form.newPassword}
+                  onChange={(e) => setForm({ ...form, newPassword: e.target.value })}></input>
+                <span className="input-group-text" style={{ cursor: "pointer" }} id="basic-addon2"
+                  onMouseUp={e => setShowNewPass(false)} onMouseDown={e => setShowNewPass(true)}><Eye size={20} /></span>
+              </div>
+            </div>
+            <div className="d-flex justify-content-end col-12">
+              <input className="btn btn-secondary m-1" type="submit" value="Сохранить" ></input>
+              <input className="btn btn-secondary m-1" type="button" value="Отмена" onClick={() => handleClose()}></input>
+            </div>
           </form>
 
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={() => handleClose()}>
-            Отмена
-            </Button>
-          <Button variant="success" onClick={() => sendForm()}>
-            Добавить
-            </Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
--- client/src/components/profile/profileinfo.js
+++ client/src/components/profile/profileinfo.js
@@ -1,72 +1,50 @@
 import React, { useState, useEffect, Text } from "react";
 import "./profileinf.scss";
 import { useHttp } from "../../hooks/http.hook";
-import { Row, Col, Alert, Container, Image, Button, Form} from "react-bootstrap";
+import { Row, Col, Alert, Container, Image, Button, Form } from "react-bootstrap";
 
 import ModalCreateUser from "./modal-create-user"
 import ModalUpdateUser from "./modal-update-user";
 
 const ProfileInfo = () => {
 
-     const { loading, error, request } = useHttp();
+    const { loading, error, request } = useHttp();
 
-     const [userNameinf, setUserName] = useState("");
-     const [firstNameinf, setFirstName] = useState("");
-     const [lastnameinf, setLastName] = useState("");
-     const [emailinf, setEmail] = useState("");
-     const [userInfo, setUserInfo] = useState(0);
-     const [roleinf, setRole] = useState("");
-     const [imageId, setImageId] = useState(null);
-     const [imageLink, setImageLink] = useState(0);
+    const [userNameinf, setUserName] = useState("");
+    const [firstNameinf, setFirstName] = useState("");
+    const [lastnameinf, setLastName] = useState("");
+    const [emailinf, setEmail] = useState("");
+    const [userInfo, setUserInfo] = useState(0);
+    const [roleinf, setRole] = useState("");
+    const [imageId, setImageId] = useState(null);
+    const [imageLink, setImageLink] = useState(0);
 
-     const [data, setData] = useState("");
+    const [data, setData] = useState("");
 
-     const [selectedFile, setSelectedFile] = useState();
-     const [isSelected, setIsSelected] = useState(false);
+    const [selectedFile, setSelectedFile] = useState();
+    const [isSelected, setIsSelected] = useState(false);
 
-     const [userId, setUserId] = useState("");
+    const [userId, setUserId] = useState("");
 
     useEffect(() => {
-        
-            getInfo();
-        
+        getInfo();
     }, []);
 
     const addDefaultSrc = (ev) => {
         ev.target.src = "https://avtokadry.infoorel.ru/images/inc/noavatar.jpg" // this could be an imported image or url
-      }
-   
+    }
 
-    //   const sendFile = async (userId, propImages) => {
-    //     const formData = new FormData();
-    //     propImages.forEach(file => {
-    //         formData.append('file', file)
-    //     });
-    //     formData.append('userId', userId)
-    //     const headers = { 'Access-Control-Allow-Credentials': 'true' }
-    //     await request(`${process.env.REACT_APP_API_URL}/uploadFiles`, "POST", formData, headers)
-    // }
+    const changeHandler = (event) => {
+        setSelectedFile(event.target.files[0]);
+        setIsSelected(true);
+    };
 
+    const handleSubmission = async (userId) => {
 
-
-
-
-	const changeHandler = (event) => {
-		setSelectedFile(event.target.files[0]);
-		setIsSelected(true);
-	};
-
-	const handleSubmission = async (userId) => {
-
-        if (imageId != null){
-            console.log(imageId);
-     
-            // await request(`${process.env.REACT_APP_API_URL}/deletePicture`, "POST", JSON.stringify(imageId));
-    
-
+        if (imageId != null) {
             await request(
                 `${process.env.REACT_APP_API_URL}/deletePicture`, 'POST', JSON.stringify(imageId)
-                
+
             )
                 .then((response) => response.json())
                 .then((result) => {
@@ -78,64 +56,53 @@ const ProfileInfo = () => {
 
         }
 
-		const formData = new FormData();
+        const formData = new FormData();
 
-		formData.append('file', selectedFile);
+        formData.append('file', selectedFile);
         formData.append('userId', userId)
-console.log(formData);
-		await fetch(
-			`${process.env.REACT_APP_API_URL}/uploadProfilePic`,
-			{
-				method: 'POST',
-				body: formData,
-			}
-		)
-			.then((response) => response.json())
-			.then((result) => {
-				console.log('Success:', result);
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
-            getInfo();
-	};
+        console.log(formData);
+        await fetch(
+            `${process.env.REACT_APP_API_URL}/uploadProfilePic`,
+            {
+                method: 'POST',
+                body: formData,
+            }
+        )
+            .then((response) => response.json())
+            .then((result) => {
+                console.log('Success:', result);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        getInfo();
+    };
 
-    
+
 
     const getInfo = async () => {
         const userData = JSON.parse(localStorage.getItem("userData"));
         try {
-            
-            const body = [
-                {
-                    "id": userData.userId
-                }
-
-            ]
-
-            console.log(body);
-            
-            
-            
-            
+            const body = [{
+                "id": userData.data.id
+            }]
             const data = await request(`${process.env.REACT_APP_API_URL}/listUsers`, "POST", JSON.stringify(body));
             const userInfo = data[0];
             setUserInfo(userInfo);
-             
+
             setEmail(userInfo.email);
             setFirstName(userInfo.firstName);
             setLastName(userInfo.lastName);
             setUserName(userInfo.userName);
             setUserId(userInfo.id);
-            console.log(userId);
-                const imagebody = 
-                            {
-                                "id": userInfo.picture.id
-                            }
+            const imagebody =
+            {
+                "id": userInfo.picture.id
+            }
 
             setImageId(imagebody);
 
-            const {link} = await request(`${process.env.REACT_APP_API_URL}/getProfilePic`, "POST", JSON.stringify(imagebody))
+            const { link } = await request(`${process.env.REACT_APP_API_URL}/getProfilePic`, "POST", JSON.stringify(imagebody))
             setImageLink(link);
 
             if (userInfo.role.roleName == "admin") {
@@ -148,64 +115,57 @@ console.log(formData);
             console.log(error);
         }
 
-       
+
     };
     if (userInfo?.role?.roleName == "admin") {
         return (
 
             <div className="profileForm">
-
                 <div className="d-flex justify-content-center">
-
-                    <div className="container p-5 ">
+                    <div className="container">
                         <div>
-                            <h3>Никнейм: {userNameinf}</h3>
+                            <p><span>Логин:</span> {userNameinf}</p>
                         </div>
                         <div>
-                            <h4>ФИО: {firstNameinf} {lastnameinf}</h4>
+                            <p><span>ФИО: </span>{firstNameinf} {lastnameinf}</p>
                         </div>
                         <div>
-                            <p>Почтовый адрес: {emailinf}</p>
+                            <p><span>Почтовый адрес:</span> {emailinf}</p>
                         </div>
                         <div>
-                            <p>Роль: Администратор</p>
+                            <p> <span>Роль:</span> Администратор</p>
                         </div>
                         <div className="form-group">
                             <ModalCreateUser />
 
                         </div>
                         <div className="form-group">
-                            <ModalUpdateUser/>
+                            <ModalUpdateUser />
 
                         </div>
-                        
-                        <div>
-                            <br />
-                            <h4>Изменить фото:</h4>
-                            <input type="file" name="file" onChange={changeHandler} />
-                            {isSelected ? (
+
+                        <div className="form-group" >
+                            <div className="custom-file col-5">
+                                <input type="file" className="custom-file-input" id="customFile" onChange={changeHandler} />
+                                <label className="custom-file-label">Изменить фото</label>
+                                {selectedFile ? <p>{selectedFile.name}</p> : null}
+                            </div>
+                            {isSelected ?
                                 <div>
-                                    <br />
-                                    <button onClick={() => handleSubmission(userId)}>Подтвердить</button>
-                                </div>
-                            ) : (
-                                <p></p>
-                            )}
-                            
-                            
+                                    <button type="button" className="btn btn-secondary mt-2" onClick={() => handleSubmission(userId)}>Подтвердить</button>
+                                </div> : null
+                            }
                         </div>
-
-
                     </div>
                     <div className="d-flex justify-content-center">
                         <Container>
                             <Row>
                                 <Col xs={22} md={11}>
-                                <Image src={imageLink} alt="" onError={addDefaultSrc} thumbnail />
+                                    <Image src={imageLink} alt="" onError={addDefaultSrc} thumbnail />
                                 </Col>
                             </Row>
                         </Container>
-                </div>
+                    </div>
                 </div>
             </div>
 
@@ -231,46 +191,45 @@ console.log(formData);
                         <div>
                             <p>Роль: Пользователь</p>
                         </div>
-                        
+
                         <div className="form-group">
-                            <ModalUpdateUser/>
-                        
+                            <ModalUpdateUser />
+
                         </div>
-                        
+
                         <div>
                             <br />
-            <h4>Изменить фото:</h4>
-			<input type="file" name="file" onChange={changeHandler} />
-			{isSelected ? (
-				<div>
-                    <br />
-                    <button onClick={() => handleSubmission(userId)}>Подтвердить</button>
-                </div>
-			) : (
-				<p></p>
-			)}
-            
-			
-		</div>
-                        
-                        
+                            <h4>Изменить фото:</h4>
+                            <input type="file" name="file" onChange={changeHandler} />
+                            {isSelected ? (
+                                <div>
+                                    <button type="button" className="btn btn-secondary" onClick={() => handleSubmission(userId)}>Подтвердить</button>
+                                </div>
+                            ) : (
+                                <p></p>
+                            )}
+
+
+                        </div>
+
+
                     </div>
                     <div className="d-flex justify-content-center">
                         <Container>
                             <Row>
                                 <Col xs={22} md={11}>
-                                <Image src={imageLink} alt="" onError={addDefaultSrc} thumbnail />
+                                    <Image src={imageLink} alt="" onError={addDefaultSrc} thumbnail />
                                 </Col>
                             </Row>
-                            
+
                         </Container>
-                        
+
+
+                    </div>
+
 
                 </div>
-                
-                    
-                </div>
-                        
+
             </div>
 
         );
